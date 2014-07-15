8e3748f41281ffc5e73acb27014a332d692a9ff4
Geovanna <geovannarodv91@gmail.com>
2014-07-15 08:30:18 -0600

    Instalacion del Bundle para editar texto

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RUGC\ProgramacionCatarsisBundle\RUGCProgramacionCatarsisBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -31,6 +32,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'\config\config_'.$this->getEnvironment().'.yml');
     }
 }
