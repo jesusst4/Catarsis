091ba94cbcb5b1dcbfa60d3a6f807e528b58433b
CSYE <jesusst4@gmail.com>
2014-08-12 16:53:54 -0600

    CRUD de enlaces

---
+++ src/RUGC/ProgramacionCatarsisBundle/Form/EnlaceType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace RUGC\ProgramacionCatarsisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EnlaceType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('descripcion')
+            ->add('enlace')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RUGC\ProgramacionCatarsisBundle\Entity\Enlace'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'rugc_programacioncatarsisbundle_enlace';
+    }
+}
